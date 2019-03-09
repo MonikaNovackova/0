d05ae96d9bf2e0f6556b50f4d8d17066845d7095
Monika Novackova <monika.novac@gmail.com>
2019-03-09 22:01:07 +0000

    starting with flask-trying to add something to my websites

---
+++ hello.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+
+app=Flask("MonikasApp")
+
+@app.route("/")
+def hello():
+    return "Hello World"
+
+@app.route("/<name>")
+def hello_someone(name):
+    return render_template("hello.html", name=name.title())
+
+app.run(debug=True)
+
+print("test")
