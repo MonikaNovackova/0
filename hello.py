cc0a7130aa4716d66f504d7865fed1d6e3930dea
Monika Novackova <monika.novac@gmail.com>
2019-03-10 02:26:17 +0000

    Playing with Flask:form

--- hello.py
+++ hello.py
@@ -1,15 +1,22 @@
 from flask import Flask, render_template, request
 
-app=Flask("MonikasApp")
+app=Flask("MyApp")
 
 @app.route("/")
-def hello():
-    return "Hello World"
+def helloW():
+    return "Hello world"
 
 @app.route("/<name>")
 def hello_someone(name):
-    return render_template("hello.html", name=name.title())
+    return render_template("hello.html",name=name.title())
+
+
+@app.route("/signup", methods=["POST"])
+def sign_up():
+    form_data=request.form
+    print form_data["email"]
+    return "All ok. Your email is:"+ form_data["email"]
+
 
 app.run(debug=True)
-
 print("test")
