963f36cadce7fae6f9adc4d045c24b78072fc6ca
Monika Novackova <monika.novac@gmail.com>
2019-03-10 18:34:45 +0000

    its working even more

--- static/js/socialM.js
+++ static/js/socialM.js
@@ -1,11 +1,11 @@
 $('#lnkin').on('click',function() {
-  window.location.href = 'https://www.linkedin.com/in/monika-novackova';
+  window.open('https://www.linkedin.com/in/monika-novackova','_blank');
 });
 
 $('#twi').on('click',function() {
-  window.location.href = 'https://twitter.com/MonikaNov2';
+  window.location.href = 'https://twitter.com/MonikaNov2' target="_blank";
 });
 
 $('#fb').on('click',function() {
-  window.location.href = 'https://www.facebook.com/monika.novac?ref=br_rs';
+  window.location.href = 'https://www.facebook.com/monika.novac?ref=br_rs' target="_blank";
 });
